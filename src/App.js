c2ba4e3f30060e60b0dc955a4ac9f85a748ff524
Badr OUAHBI <badii_00@hotmail.com>
2023-03-05 20:17:13 +0100

    API KEY protection

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ import Map from "./pages/map/map";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDMDVqnLBz21BLJCBDcABRX6K8BWG_KbIc",
-  authDomain: "global-bim.firebaseapp.com",
-  projectId: "global-bim",
-  storageBucket: "global-bim.appspot.com",
-  messagingSenderId: "577252476037",
-  appId: "1:577252476037:web:09fb33abb49eea0f35d5ec"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
