95f1444ef23b94d802f67c1e34a792f144b900ff
Badr OUAHBI <badii_00@hotmail.com>
2023-03-05 19:12:02 +0100

    Connect firebase project

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LoginForm } from "./components/login-form";
 import { BuildingViewer } from "./components/building-viewer";
 import { MapViewer } from "./components/map-viewer";
+import { initializeApp } from "firebase/app";
 
 // page components
 import Navbar from "./components/Navbar";
 import Map from "./pages/map/map";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDMDVqnLBz21BLJCBDcABRX6K8BWG_KbIc",
+  authDomain: "global-bim.firebaseapp.com",
+  projectId: "global-bim",
+  storageBucket: "global-bim.appspot.com",
+  messagingSenderId: "577252476037",
+  appId: "1:577252476037:web:09fb33abb49eea0f35d5ec"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 function App() {
   return (
