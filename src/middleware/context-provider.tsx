86500a90fd70ff48288e9130c301d433a1b5b5bd
Badr OUAHBI <badii_00@hotmail.com>
2023-03-06 21:40:06 +0100

    Setup Middleware

---
+++ src/middleware/context-provider.tsx
@@ -0,0 +1,38 @@
+import {
+    createContext,
+    useReducer,
+    FC,
+    PropsWithChildren,
+    useContext,
+  } from "react";
+  import { Action } from "./actions";
+  import { reducer } from "./state-handler";
+  import { initialState, State } from "./state";
+  import { Authenticator } from "./authenticator";
+  import { executeCore } from "./core-handler";
+  
+  const appContext = createContext<[State, React.Dispatch<Action>]>([
+    initialState,
+    () => {},
+  ]);
+  
+  export const ContextProvider: FC<PropsWithChildren> = ({ children }) => {
+    const [state, setState] = useReducer(reducer, initialState);
+  
+    const dispatch = (value: Action) => {
+      setState(value);
+      executeCore(value);
+    };
+  
+    return (
+      <appContext.Provider value={[state, dispatch]}>
+        <Authenticator />
+        {children}
+      </appContext.Provider>
+    );
+  };
+  
+  export const useAppContext = () => {
+    return useContext(appContext);
+  };
+  
